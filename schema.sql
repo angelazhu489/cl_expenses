210f641fbceaf408050b65caa988f1da380ae325
angelazhu489 <angelazhu489@gmail.com>
2024-05-04 17:52:17 -0700

    setup

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE expenses;
+
+CREATE TABLE expenses (
+	id serial PRIMARY KEY,
+	amount numeric(6, 2) NOT NULL CHECK(amount > 0),
+	memo text NOT NULL,
+	created_on date NOT NULL
+);
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
